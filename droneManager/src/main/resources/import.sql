5caacbf9aee92e6e67f6a874d53913e172a6f402
guira12 <ayman.slh12@gmail.com>
2025-12-11 13:56:00 +0100

    minimal endpoint for testing purposes

--- droneManager/src/main/resources/import.sql
+++ droneManager/src/main/resources/import.sql
@@ -1,3 +1,7 @@
+/*
+    pilot inserts
+*/
+
 INSERT INTO pilot (id, first_name, last_name, licence_number) VALUES ('3fa85f64-5717-4562-b3fc-2c963f66afa6', 'Laurent', 'GINA', 'LN-001');
 INSERT INTO pilot (id, first_name, last_name, licence_number) VALUES ('3fa85f64-5717-4562-b3fc-2c963f66afa7', 'Sophie', 'FONCEK', 'LN-002');
 INSERT INTO pilot (id, first_name, last_name, licence_number) VALUES ('3fa85f64-5717-4562-b3fc-2c963f66afa8', 'Agathe', 'FEELING', 'LN-003');
